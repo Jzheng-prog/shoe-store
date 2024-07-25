a3135fda8d8b6b2972d97a07af8a927f5638fd6e
Jzheng-prog <zhengjohn017@gmail.com>
2024-07-25 02:22:03 -0400

    minor changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,8 +20,11 @@ const Hero = () => {
       <div className='opacity-0 translate-y-20 border-b mb-6' id="jordan">
         <h2 className='text-3xl font-bold text-gray-900 mb-4' >Jordans</h2>
         <div className='flex'>
-          <p className='w-[400px]'>Step into the world of iconic athletic wear with our <span className='font-bold text-lg'>exclusive</span> Jordan collection. Renowned for revolutionizing the world of sports and fashion, Jordan sneakers are not just shoes; they're a symbol of performance and style. Born from the legendary partnership between Michael Jordan and Nike, each pair of Jordans tells a story of excellence and innovation.</p>
-         <img src="/jordan-1-red.webp" alt="jordan-1-spiderman"  className=''/>
+          <p className='w-[400px] mt-5 p-2'>Step into the world of iconic athletic wear with our <span className='font-bold text-lg'>exclusive</span> Jordan collection. Renowned for revolutionizing the world of sports and fashion, Jordan sneakers are not just shoes; they're a symbol of performance and style. Born from the legendary partnership between Michael Jordan and Nike, each pair of Jordans tells a story of excellence and innovation.</p>
+          <div>
+            <img src="/jordan-1-red.webp" alt="jordan-1-spiderman"/>
+            <p className='font-bold text-lg justify-center flex'>Nike Air Jordan 1 Retro High OG</p>
+          </div>
         </div>
       </div>
       {/* Slides */}
@@ -29,7 +32,7 @@ const Hero = () => {
       <div>
         <h2 className='text-3xl font-bold text-gray-900 mb-4' >Slides</h2>
           <div className='flex'>
-            <p className='w-[400px]'>Discover the perfect pair of Yeezy slides that suits your style and needs. Embrace the fusion of comfort, durability, and high-fashion with our exclusive collection, and elevate your everyday look with a touch of Yeezy innovation.</p>
+            <p className='w-[400px] mt-5 p-2'>Discover the perfect pair of Yeezy slides that suits your style and needs. Embrace the fusion of comfort, durability, and high-fashion with our exclusive collection, and elevate your everyday look with a touch of Yeezy innovation.</p>
             <img src="/onyx-slide.png" alt="onxy-slide"  className='w-[1000px]'/>
           </div>
       </div>
