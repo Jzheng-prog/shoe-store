4bbdd21ec69180ade2cc76dd75771895b744f59b
Jzheng-prog <74681328+Jzheng-prog@users.noreply.github.com>
2024-08-10 21:55:18 -0400

    mac

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import Heading from "@/components/Heading";
+import React, { useContext } from "react";
+import GlobalContext from "@/context";
 
-const Favorites = () => {
-  return (
-    <div>
-      favorites
-    </div>
-  )
+interface GlobalContextType {
+  favoriteList: string[];
 }
 
-export default Favorites
+const Favorites: React.FC = () => {
+  const favoriteList = [
+    {
+      id: 1,
+      title: "Jordan-1",
+      img: "/jordan.svg",
+      description: "iiii",
+    },
+    {
+      id: 2,
+      title: "Jordan-1",
+      img: "/jordan.svg",
+      description: "iiii",
+    },
+    {
+      id: 3,
+      title: "Jordan-1",
+      img: "/jordan.svg",
+      description: "iiii",
+    },
+  ];
+  return (
+    <div>
+      <Heading title={"Favorites"} />
+
+      {favoriteList && favoriteList.length > 0 ? (
+        <div>
+          {favoriteList.map((item, index) => (
+            <div key={index}>{item.id}</div>
+          ))}
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
+
+export default Favorites;
