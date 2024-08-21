1b6e4e8a3dbf3a954b9e09591d503e189d2eaf36
Jzheng-prog <74681328+Jzheng-prog@users.noreply.github.com>
2024-08-20 21:09:05 -0400

    push

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,8 @@
 import { FormEvent, useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
+import Heading from "@/components/Heading";
+import LoginForm from "@/components/account/LoginForm";
 
 export default function Login() {
   const [error, setError] = useState("");
@@ -27,40 +28,10 @@ export default function Login() {
 
   return (
     <section className="w-full h-screen flex items-center justify-center">
-      <form
-        className="p-6 w-full max-w-[400px] flex flex-col justify-between items-center gap-2 
-        border border-solid border-black bg-white rounded"
-        onSubmit={handleSubmit}
-      >
-        {error && <div className="text-black">{error}</div>}
-        <h1 className="mb-5 w-full text-2xl font-bold">Sign In</h1>
-        <label className="w-full text-sm">Email</label>
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full h-8 border border-solid border-black rounded p-2"
-          name="email"
-        />
-        <label className="w-full text-sm">Password</label>
-        <div className="flex w-full">
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full h-8 border border-solid border-black rounded p-2"
-            name="password"
-          />
-        </div>
-        <button className="w-full border border-solid border-black rounded">
-          Sign In
-        </button>
-
-        <Link
-          href="/register"
-          className="text-sm text-[#888] transition duration-150 ease hover:text-black"
-        >
-          Dont have an account?
-        </Link>
-      </form>
+      <div>
+        <Heading title="ShoeStore" />
+        <LoginForm handleSubmit={handleSubmit} error={error} />
+      </div>
     </section>
   );
 }
---
+++ components/account/LoginForm.tsx
@@ -0,0 +1,50 @@
+import React, { FormEvent } from "react";
+import Link from "next/link";
+
+interface LoginFormProps {
+  handleSubmit: (event: FormEvent<HTMLFormElement>) => void;
+  error: string;
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ handleSubmit, error }) => {
+  return (
+    <div>
+      <form
+        className="p-6 w-full max-w-[400px] flex flex-col justify-between items-center gap-2 
+        border border-solid border-black bg-white rounded"
+        onSubmit={handleSubmit}
+      >
+        {error && <div className="text-black">{error}</div>}
+        <h1 className="mb-5 w-full text-2xl font-bold">Sign In</h1>
+        <label className="w-full text-sm">Email</label>
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full h-8 border border-solid border-black rounded p-2"
+          name="email"
+        />
+        <label className="w-full text-sm">Password</label>
+        <div className="flex w-full">
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full h-8 border border-solid border-black rounded p-2"
+            name="password"
+          />
+        </div>
+        <button className="w-full border border-solid border-black rounded">
+          Sign In
+        </button>
+
+        <Link
+          href="/register"
+          className="text-sm text-[#888] transition duration-150 ease hover:text-black"
+        >
+          Dont have an account?
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
